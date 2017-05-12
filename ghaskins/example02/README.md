c1d89cbb9b27ba7fdf95849bdf7593b157f359f5
Gregory Haskins <gregory.haskins@gmail.com>
2017-05-12 13:58:34 -0400

    Add ghaskins/example02
    Signed-off-by: Gregory Haskins <gregory.haskins@gmail.com>

---
+++ ghaskins/example02/README.md
@@ -0,0 +1,10 @@
+# ghaskins/example02
+This directory contains a chaincode application inspired by "example02" found in the [fabric.git](https://github.com/hyperledger/fabric/tree/master/examples/chaincode/go/chaincode_example02) repository.
+
+## Technologies Used
+* chaincode: [chaintool](https://github.com/hyperledger/fabric-chaintool) based golang
+* client: nodejs command line application written with the [fabric-sdk-node](https://github.com/hyperledger/fabric-sdk-node)
+
+## Status
+
+This example is still a _Work In Progress_
---
+++ ghaskins/example02/client/client.js
@@ -0,0 +1,243 @@
+var program = require('commander');
+var pb = require("protobufjs");
+
+var builder = pb.newBuilder({ convertFieldsToCamelCase: true });
+
+pb.loadProtoFile("./protos/appinit.proto", builder);
+var init = builder.build("appinit");
+
+pb.loadProtoFile("./protos/org.hyperledger.chaincode.example02.proto", builder);
+var app = builder.build("org.hyperledger.chaincode.example02");
+
+var path = require('path');
+
+var hfc = require('fabric-client');
+var hfcutils = require('fabric-client/lib/utils.js');
+var utils = require('./lib/util.js');
+var Peer = require('fabric-client/lib/Peer.js');
+var Orderer = require('fabric-client/lib/Orderer.js');
+var EventHub = require('fabric-client/lib/EventHub.js');
+var CA = require('fabric-ca-client');
+var User = require('fabric-client/lib/User.js');
+
+var client;
+var chain;
+var peer;
+var eventhub;
+
+var chainId = 'mychannel';
+
+function createBaseRequest(user) {
+    var nonce = hfcutils.getNonce();
+    var tx_id = hfc.buildTransactionID(nonce, user);
+
+    // send proposal to endorser
+    var request = {
+        chaincodeType: 'car',
+        targets: [peer],
+        chainId: chainId,
+        chaincodeId: 'mycc',
+        txId: tx_id,
+        nonce: nonce
+    };
+
+    return request;
+}
+
+function createRequest(user, fcn, args) {
+    var request = createBaseRequest(user);
+
+    request.fcn = fcn;
+    request.args = [args.toBuffer()];
+
+    return request;
+}
+
+function makeUrl(host, port) {
+    return 'grpc:' + host + ':' + port;
+ }
+
+function connect(config) {
+    client = new hfc();
+    chain = client.newChain(chainId);
+
+    eventhub = new EventHub();
+    eventhub.setPeerAddr(makeUrl(config.peer.address, config.peer.events));
+    eventhub.connect();
+
+    peer = new Peer(makeUrl(config.peer.address, config.peer.port));
+    var orderer = new Orderer(config.orderer);
+
+    chain.addOrderer(orderer);
+    chain.addPeer(peer);
+
+    return utils.setStateStore(client, ".hfc-kvstore")
+        .then(() => {
+            var ca = new CA(config.ca);
+
+            return utils.getUser(client, ca, config.username, config.password);
+        })
+        .then((user) => {
+            return chain.initialize()
+                .then(() => {
+
+                    return user;
+                });
+        });
+}
+
+function disconnect() {
+    return new Promise((resolve, reject) => {
+        eventhub.disconnect();
+        resolve();
+    });
+}
+
+function sendInstall(user, path, version) {
+
+    var request = createBaseRequest(user);
+    if (path) {
+        request.chaincodePath = path;
+    } else {
+        chain.setDevMode(true);
+    }
+
+    console.log(version);
+    request.chaincodeVersion = "1";
+
+    // send proposal to endorser
+    return client.installChaincode(request);
+}
+
+function sendInstantiate(user, args) {
+
+    var request = createRequest(user, 'init', new init.Init(args));
+    request.chaincodeVersion = "1";
+
+    // send proposal to endorser
+    return chain.sendInstantiateProposal(request)
+        .then((response) => {
+            return utils.processResponse(chain, eventhub, request, response, 60000);
+        });
+}
+
+function sendTransaction(user, fcn, args) {
+
+    var request = createRequest(user, fcn, args);
+
+    return chain.sendTransactionProposal(request)
+        .then((response) => {
+            return utils.processResponse(chain, eventhub, request, response, 20000);
+        });
+}
+
+function sendQuery(user, fcn, args) {
+    var request = createRequest(user, fcn, args);
+    return chain.queryByChaincode(request);
+}
+
+function makePayment(user, args) {
+    return sendTransaction(user,
+                           'org.hyperledger.chaincode.example02/fcn/1',
+                           new app.PaymentParams(args));
+}
+
+function checkBalance(user, args) {
+    return sendQuery(user,
+                     'org.hyperledger.chaincode.example02/fcn/3',
+                     new app.Entity(args))
+        .then((results) => {
+            return app.BalanceResult.decode(results[0]);
+        });
+}
+
+var defaultConfig = {
+    ca: 'http://172.18.0.2:7054',
+    orderer: 'grpc://172.18.0.3:7050',
+    peer: {
+        address: '172.18.0.4',
+        port: 7051,
+        events: 7053
+    },
+    mspid: 'Org1MSP',
+    username: 'admin',
+    password: 'adminpw'
+};
+
+program
+    .version('0.0.1');
+
+program
+    .command('install')
+    .option("-p, --path <path>", "Path to chaincode.car")
+    .option("-v, --version <version>", "Version of chaincode to install")
+    .action((options) => {
+        return connect(defaultConfig)
+            .then((user) => {
+                return sendInstall(user, options.path, options.version);
+            })
+            .then(() => {
+                return disconnect();
+            })
+            .catch((err) => {
+                console.log("error:" + err);
+            });
+    });
+
+program
+    .command('instantiate')
+    .action(() => {
+        return connect(defaultConfig)
+            .then((user) => {
+                return sendInstantiate(user,
+                                       {
+                                           'partyA': {'entity':'A', 'value':100},
+                                           'partyB': {'entity':'B', 'value':200}
+                                       });
+            })
+            .then(() => {
+                return disconnect();
+            })
+            .catch((err) => {
+                console.log("error:" + err);
+            });
+    });
+
+program
+    .command('makepayment <partySrc> <partyDst> <amount>')
+    .action((partySrc, partyDst, amount) => {
+        return connect(defaultConfig)
+            .then((user) => {
+                return makePayment(user,
+                                   {
+                                       'partySrc': partySrc,
+                                       'partyDst': partyDst,
+                                       'amount':   parseInt(amount)
+                                   });
+            })
+            .then(() => {
+                return disconnect();
+            })
+            .catch((err) => {
+                console.log("error:" + err);
+            });
+    });
+
+program
+    .command('checkbalance <id>')
+    .action((id) => {
+        return connect(defaultConfig)
+            .then((user) => {
+                return checkBalance(user, {'id':id});
+            })
+            .then((result) => {
+                console.log("balance:" + result.balance);
+                return disconnect();
+            })
+            .catch((err) => {
+                console.log("error:" + err);
+            });
+    });
+
+
+program.parse(process.argv);
